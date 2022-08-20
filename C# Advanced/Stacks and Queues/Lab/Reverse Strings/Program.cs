c39aaab9713ec7abbbb97685fecc08e065e943be
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-20 14:58:13 +0300

    Add files via upload

---
+++ C# Advanced/Stacks and Queues/Lab/Reverse Strings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Reverse_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            var stack = new Stack<char>();
+            
+            for (int i = 0; i < text.Length; i++)
+            {
+                stack.Push(text[i]);
+            }
+
+            Console.WriteLine(string.Join("", stack));
+        }
+    }
+}
