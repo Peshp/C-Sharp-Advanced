c6671728336880effad6e268602135aab48addcc
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-20 15:00:16 +0300

    Add files via upload

---
+++ C# Advanced/Stacks and Queues/Lab/Matching Brackets/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Matching_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if(text[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                else if(text[i] == ')')
+                {
+                    int startIndex = stack.Pop();
+                    string result = text.Substring(startIndex, i - startIndex + 1);
+                    Console.WriteLine(result);
+                }
+            }
+        }
+    }
+}
