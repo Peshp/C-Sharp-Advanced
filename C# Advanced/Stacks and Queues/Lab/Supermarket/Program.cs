53a584bbad6645455743407a4d6c9c7057a378e0
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-21 14:33:29 +0300

    Add files via upload

---
+++ C# Advanced/Stacks and Queues/Lab/Supermarket/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            var names = new Queue<string>();
+
+            while (true)
+            {
+                string name = Console.ReadLine();
+                if(name == "End")
+                {
+                    break;
+                }
+
+                if (name == "Paid")
+                {
+                    while (names.Count > 0)
+                    {
+                        Console.WriteLine(names.Dequeue());
+                    }                    
+                }
+                else
+                {
+                    names.Enqueue(name);
+                }                            
+            }
+           
+            Console.WriteLine($"{names.Count} people remaining.");
+        }
+    }
+}
