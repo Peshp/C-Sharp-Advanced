91f90f7e7dcf4d78b12b1239b7bf36b4f3019be5
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-21 14:34:10 +0300

    Add files via upload

---
+++ C# Advanced/Stacks and Queues/Lab/Hot Potato/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hot_Potato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split();
+            var potato = new Queue<string>(names);
+            int n = int.Parse(Console.ReadLine());
+
+            while (potato.Count > 1)
+            {
+                for (int i = 1; i <= n - 1; i++)
+                {
+                    var player = potato.Dequeue();
+                    potato.Enqueue(player);
+                }
+                Console.WriteLine($"Removed {potato.Dequeue()}");
+            }
+            Console.WriteLine($"Last is {potato.Dequeue()}");
+        }
+    }
+}
