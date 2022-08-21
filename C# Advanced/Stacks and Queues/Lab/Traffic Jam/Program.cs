33e20c53af170bd7c86ed4e2cde0125b57e59ab7
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-21 14:34:56 +0300

    Add files via upload

---
+++ C# Advanced/Stacks and Queues/Lab/Traffic Jam/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Traffic_Jam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string car = "";
+            var pass = new Queue<string>();
+            int passed = 0;
+
+            while (car != "end")
+            {
+                car = Console.ReadLine();
+                          
+                if(car == "green")
+                {
+                    for (int i = 1; i <= n; i++)
+                    {                       
+                        if(pass.Count > 0)
+                        {
+                            Console.WriteLine($"{pass.Dequeue()} passed!");
+                            passed++;
+                        }
+                    }
+                }
+                else
+                {
+                    pass.Enqueue(car);
+                }
+            }
+            Console.WriteLine($"{passed} cars passed the crossroads.");
+        }
+    }
+}
