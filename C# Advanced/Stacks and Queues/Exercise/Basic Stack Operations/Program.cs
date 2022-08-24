d28bf4d6b3a50b7d49464ba7230dccb044729f37
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-24 15:33:55 +0300

    Add files via upload

---
+++ C# Advanced/Stacks and Queues/Exercise/Basic Stack Operations/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Basic_Stack_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nsx = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = nsx[0];
+            int s = nsx[1];
+            int x = nsx[2];
+            var stack = new Stack<int>();
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < n; i++)
+            {
+                stack.Push(nums[i]);
+            }
+
+            for (int i = 0; i < s; i++)
+            {
+                stack.Pop();
+            }
+
+            if(stack.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else if(stack.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(stack.Min());
+            }
+        }
+    }
+}
