0d5d9dd88f9982009a4a856d4916ce92bf3e615f
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-24 15:34:37 +0300

    Add files via upload

---
+++ C# Advanced/Stacks and Queues/Exercise/Basic Queue Operations/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Basic_Queue_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nsx = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = nsx[0];
+            int s = nsx[1];
+            int x = nsx[2];
+
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var queue = new Queue<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                queue.Enqueue(nums[i]);
+            }
+
+            for (int i = 0; i < s; i++)
+            {
+                queue.Dequeue();
+            }
+
+            if(queue.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else if(queue.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(queue.Min());
+            }
+        }
+    }
+}
