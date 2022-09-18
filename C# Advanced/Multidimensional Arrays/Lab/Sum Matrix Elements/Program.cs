c20eeb07f252b8abcedf209e90fd7d3628556600
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-09-18 16:37:36 +0300

    Add files via upload

---
+++ C# Advanced/Multidimensional Arrays/Lab/Sum Matrix Elements/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Sum_Matrix_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizes = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int[,] matrix = new int[sizes[0], sizes[1]];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int[] elements = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = elements[j];
+                }
+            }
+
+            int sum = 0;
+            foreach (var item in matrix)
+            {
+                sum += item;
+            }
+
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            Console.WriteLine(sum);
+        }
+    }
+}
