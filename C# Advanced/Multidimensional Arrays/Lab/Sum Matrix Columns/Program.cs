7ed3441024443d4511fd80f241b0dcaec3a8f862
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-09-18 16:38:17 +0300

    Add files via upload

---
+++ C# Advanced/Multidimensional Arrays/Lab/Sum Matrix Columns/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Sum_Matrix_Columns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int[,] matrix = new int[size[0], size[1]];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = a[j];
+                }
+            }
+
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < matrix.GetLength(0); j++)
+                {
+                    sum += matrix[j, i];
+                }
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
