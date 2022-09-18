75ead26f8fd3ae10d22f98d4a038fa1aa8ef7d41
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-09-18 16:39:44 +0300

    Add files via upload

---
+++ C# Advanced/Multidimensional Arrays/Lab/Symbol in Matrix/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Symbol_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                string ascii = Console.ReadLine();
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = ascii[j];
+                }
+            }
+            char symbol = char.Parse(Console.ReadLine());
+            string result = ISContainsSymbol(matrix, symbol);
+            Console.WriteLine(result);
+        }
+
+        static string ISContainsSymbol(int[,] matrix, char symbol)
+        {
+            string result = "";
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    char g = (char)matrix[i, j];
+                    if (symbol == g)
+                    {
+                        result = $"({i}, {j})";
+                        return result;
+                    }
+                }
+            }
+            result = $"{symbol} does not occur in the matrix";
+            return result;
+        }
+    }
+}
