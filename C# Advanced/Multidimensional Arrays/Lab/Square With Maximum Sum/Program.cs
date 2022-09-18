f00ee168b2f3aedf2c6ae0e96060ad58347664fa
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-09-18 16:40:25 +0300

    Add files via upload

---
+++ C# Advanced/Multidimensional Arrays/Lab/Square With Maximum Sum/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        int a = input.Split(", ").Select(int.Parse).First();
+        int b = input.Split(", ").Select(int.Parse).Last();
+        int[,] matrix = new int[a, b];
+        for (int i = 0; i < a; i++)
+        {
+            int[] n = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            for (int j = 0; j < b; j++)
+            {
+                matrix[a, b] = n[j];
+            }
+        }
+
+        int max = int.MinValue;
+        int bigRow = -1;
+        int bigCol = -1;
+        for (int i = 0; i < a - 1; i++)
+        {
+            for (int j = 0; j < b - 1; j++)
+            {
+                int sum = matrix[i, j]
+                    + matrix[i, j + 1]
+                    + matrix[i + 1, j]
+                    + matrix[i + 1, j + 1];
+
+                if (sum > max)
+                {
+                    max = sum;
+                    bigRow = i;
+                    bigCol = j;
+                }
+            }
+        }
+
+        Console.WriteLine($"{matrix[bigRow, bigCol]} {matrix[bigCol, bigRow + 1]}");
+        Console.WriteLine($"{matrix[bigRow + 1, bigCol]} {matrix[bigRow + 1, bigCol + 1]}");
+        Console.WriteLine(max);
+    }
+}
