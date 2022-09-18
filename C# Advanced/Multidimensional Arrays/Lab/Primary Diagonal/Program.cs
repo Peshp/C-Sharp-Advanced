611081cae37e3a9a96db0754e062045870a8483e
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-09-18 16:39:04 +0300

    Add files via upload

---
+++ C# Advanced/Multidimensional Arrays/Lab/Primary Diagonal/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Primary_Diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = a[j];
+                }
+            }
+
+            int sum = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if(i == j)
+                    {
+                        sum += matrix[i, j];
+                    }                   
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
