e6829082a80300553eb95bb15e1165c3708fcc1a
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-09-22 19:05:05 +0300

    Add files via upload

---
+++ C# Advanced/Multidimensional Arrays/Exercise/Diagonal Difference/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace Diagonal_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[size, size];
+
+            for (int i = 0; i < size; i++)
+            {
+                int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int j = 0; j < size; j++)
+                {
+                    matrix[i, j] = nums[j];
+                }
+            }
+
+            int first = 0;
+            int second = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                first += matrix[i, i];
+                second += matrix[i, size - i - 1];
+            }
+
+            Console.WriteLine(Math.Abs(first - second));        
+        }
+    }
+}
