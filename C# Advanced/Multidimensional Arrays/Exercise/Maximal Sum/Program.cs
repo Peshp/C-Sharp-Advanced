e2ffa7c12bb33e86924716dea5a74e91d3a31771
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-09-22 19:06:41 +0300

    Add files via upload

---
+++ C# Advanced/Multidimensional Arrays/Exercise/Maximal Sum/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+
+namespace Maximal_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizes = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();         
+
+            int[,] matrix = new int[sizes[0], sizes[1]];
+            MatrixMetgod(matrix);
+
+            int maxSum = int.MinValue;
+            int maxRow = 0;
+            int maxCol = 0;
+            for (int i = 0; i < matrix.GetLength(0) - 2; i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1) - 2; j++)
+                {
+                    int curr = matrix[i, j] + matrix[i, j + 1] + matrix[i, j + 2] +
+                        matrix[i + 1, j] + matrix[i + 1, j + 1] + matrix[i + 1, j + 2] +
+                        matrix[i + 2, j] + matrix[i + 2, j + 1] + matrix[i + 2, j + 2];
+
+                    if (curr > maxSum)
+                    {
+                        maxSum = curr;
+                        maxRow = i;
+                        maxCol = j;
+                    }
+                }
+            }
+            Console.WriteLine($"Sum = {maxSum}");
+
+            for (int i = maxRow; i < maxRow + 3 ; i++)
+            {
+                for (int j = maxCol; j < maxCol + 3; j++)
+                {
+                    Console.Write($"{matrix[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+        private static int[,] MatrixMetgod(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int[] nums = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = nums[j];
+                }
+            }
+            return matrix;
+        }
+    }
+}
