8822b13a623fd9e3ce1f3fc19dc8842e072388b1
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-09-26 19:58:02 +0300

    Add files via upload

---
+++ C# Advanced/Sets and Dictionaries Advanced/Lab/Largest 3 Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Largest_3_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            numbers.Sort();
+            if(numbers.Count > 3)
+                Console.WriteLine(string.Join(" ", numbers.OrderByDescending(x => x).Take(3)));
+            else
+                Console.WriteLine(string.Join(" ", numbers.OrderByDescending(x => x)));
+        }
+    }
+}
