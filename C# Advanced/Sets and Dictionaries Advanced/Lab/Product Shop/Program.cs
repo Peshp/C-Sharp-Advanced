ccdc6dcd899e4458f801b9bf5150eb3bc426b1c2
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-09-26 19:58:46 +0300

    Add files via upload

---
+++ C# Advanced/Sets and Dictionaries Advanced/Lab/Product Shop/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Product_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var products = new Dictionary<string, Dictionary<string, double>>();           
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "Revision")
+                    break;
+                string[] token = command.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (!products.ContainsKey(token[0]))
+                {
+                    products.Add(token[0], new Dictionary<string, double>());
+                }
+                products[token[0]].Add(token[1], double.Parse(token[2]));
+            }
+
+            foreach (var item in products.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{item.Key}->");
+                Console.WriteLine(string.Join("\n", item.Value.Select(x => $"Product: {x.Key}, Price: {x.Value}")));
+            }
+        }
+    }
+}
