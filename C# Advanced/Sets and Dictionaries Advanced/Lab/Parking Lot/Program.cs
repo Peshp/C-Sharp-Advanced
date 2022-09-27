ceb892727ccf012988e7eed5e0b7a3913aa9c855
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-09-27 20:31:15 +0300

    Add files via upload

---
+++ C# Advanced/Sets and Dictionaries Advanced/Lab/Parking Lot/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Parking_Lot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = "";
+            var cars = new HashSet<string>();
+
+            while (command != "END")
+            {
+                command = Console.ReadLine();
+                string[] token = command.Split(", ");
+
+                switch (token[0])
+                {
+                    case "IN":
+                        cars.Add(token[1]);
+                        break;
+                    case "OUT":
+                        cars.Remove(token[1]);
+                        break;
+                }
+            }
+
+            if(cars.Count > 0)
+                foreach (var item in cars)              
+                    Console.WriteLine(item);   
+            else
+                Console.WriteLine("Parking Lot is Empty");
+        }
+    }
+}
