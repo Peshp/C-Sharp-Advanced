de07864b3e7748547b9f8f4f9beb3196abbe8515
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-09-27 20:31:56 +0300

    Add files via upload

---
+++ C# Advanced/Sets and Dictionaries Advanced/Lab/SoftUni Party/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SoftUni_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var guests = new HashSet<string>();  
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "END")
+                {
+                    break;
+                }
+                if (command == "PARTY")
+                {
+                    while (true)
+                    {
+                        guests.Remove("PARTY");
+                        string members = Console.ReadLine();
+                        if (members == "END")
+                            break;
+                        if (guests.Contains(members))
+                        {
+                            guests.Remove(members);
+                        }
+                    }
+                    break;
+                }               
+                guests.Add(command);            
+            }
+            Console.WriteLine(guests.Count);
+            foreach (var item in guests.OrderBy(x => (int)x[0]))
+                Console.WriteLine(item);
+        }
+    }
+}
