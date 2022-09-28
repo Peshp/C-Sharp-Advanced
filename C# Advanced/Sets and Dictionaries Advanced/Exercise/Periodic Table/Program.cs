5e211dc1f44aea620478be86832e119260f3659c
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-09-28 21:49:00 +0300

    Add files via upload

---
+++ C# Advanced/Sets and Dictionaries Advanced/Exercise/Periodic Table/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var chemical = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] elements = Console.ReadLine().Split();
+                for (int j = 0; j < elements.Length; j++)
+                {
+                    chemical.Add(elements[j]);
+                }
+            }
+            Console.WriteLine(string.Join(" ", chemical));
+        }
+    }
+}
