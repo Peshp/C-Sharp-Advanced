b506703a38a246597f01256c502715c2a8e504f9
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-09-28 21:47:41 +0300

    Add files via upload

---
+++ C# Advanced/Sets and Dictionaries Advanced/Exercise/Unique Usernames/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Unique_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var unique = new HashSet<string>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)                        
+                unique.Add(Console.ReadLine());
+
+            foreach (var item in unique)
+                Console.WriteLine(item);
+        }
+    }
+}
