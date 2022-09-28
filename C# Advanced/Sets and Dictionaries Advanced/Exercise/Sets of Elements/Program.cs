0148556d1bbdb50867de0ab4841382635d572061
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-09-28 21:48:21 +0300

    Add files via upload

---
+++ C# Advanced/Sets and Dictionaries Advanced/Exercise/Sets of Elements/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sets_of_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nm = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var nElements = new HashSet<int>();
+            var mElements = new HashSet<int>();
+
+            for (int i = 0; i < nm[0]; i++)           
+                nElements.Add(int.Parse(Console.ReadLine()));
+            for (int i = 0; i < nm[1]; i++)
+                mElements.Add(int.Parse(Console.ReadLine()));
+
+            nElements.IntersectWith(mElements);
+            Console.WriteLine(string.Join(" ", nElements));
+        }
+    }
+}
