96d521d968b254c1ea03d59ba7939734fa1e4038
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-09-29 22:34:13 +0300

    Add files via upload

---
+++ C# Advanced/Sets and Dictionaries Advanced/Exercise/Wardrobe/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Wardrobe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var clothes = new Dictionary<string, Dictionary<string, int>>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string line = Console.ReadLine();
+                string color = line.Split(" -> ").First();
+                string[] shop = line.Split(" -> ").Last().Split(",").ToArray();
+
+                if (!clothes.ContainsKey(color))              
+                    clothes.Add(color, new Dictionary<string, int>());
+                
+                for (int j = 0; j < shop.Length; j++)
+                {
+                    if (!clothes[color].ContainsKey(shop[j]))
+                        clothes[color].Add(shop[j], 0);
+                    clothes[color][shop[j]]++;
+                }
+            }
+            string input = Console.ReadLine();
+            string search = input.Split()[0];
+            string look = input.Split()[1];
+
+            foreach (var item in clothes)
+            {
+                Console.WriteLine($"{item.Key} clothes:");
+                foreach (var cloth in item.Value)
+                {                   
+                    if(search == item.Key && look == cloth.Key)
+                        Console.WriteLine($"* {cloth.Key} - {cloth.Value} (found!)");
+                    else
+                        Console.WriteLine($"* {cloth.Key} - {cloth.Value}");
+                }
+            }
+        }
+    }
+}
