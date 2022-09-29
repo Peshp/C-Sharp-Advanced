2b68083eadaacdfa1d95da9a1e91a72329823a23
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-09-29 22:33:32 +0300

    Add files via upload

---
+++ C# Advanced/Sets and Dictionaries Advanced/Exercise/Count Symbols 1 ​/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Count_Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var counter = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char symbol = input[i];
+                if (!counter.ContainsKey(symbol))
+                    counter.Add(symbol, 0);
+                counter[symbol]++;
+            }
+            foreach (var item in counter)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}
