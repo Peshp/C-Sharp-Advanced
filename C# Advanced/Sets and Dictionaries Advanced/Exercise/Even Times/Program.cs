950797b934ae554722c0a60c8a3aa75e64a0dd52
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-09-29 22:30:18 +0300

    Add files via upload

---
+++ C# Advanced/Sets and Dictionaries Advanced/Exercise/Even Times/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Even_Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var nums = new HashSet<int>();
+            int count = 1;
+            int even = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());                
+                if(nums.Contains(num))
+                {
+                    count++;
+                    if(count == 3)
+                    {
+                        count = 1;
+                    }
+                    if (count % 2 == 0)
+                    {
+                        even = num;                      
+                    }
+                }
+                nums.Add(num);
+            }
+            Console.WriteLine(even);
+        }
+    }
+}
