3b93d5039b27a1f9877ebed46a6b3e33b89f5740
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-10-05 19:07:42 +0300

    Add files via upload

---
+++ C# Advanced/Functional Programming/Lab/Count Uppercase Words/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Count_Uppercase_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<string> checker = n => n[0] == n.ToUpper()[0];
+            Console.WriteLine(string.Join("\n",Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).
+                Where(w => checker(w)).ToArray()));
+        }
+    }
+}
