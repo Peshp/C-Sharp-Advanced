da9665a1780ab13ae117fb40f80df8a57f4b73ab
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-10-05 19:06:27 +0300

    Add files via upload

---
+++ C# Advanced/Functional Programming/Lab/Sort Even Numbers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace Sort_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(", ", Console.ReadLine().Split(", ").Select(n => int.Parse(n)).
+            Where(n => n % 2 == 0).OrderBy(n => n).ToArray()));
+        }
+    }
+}
