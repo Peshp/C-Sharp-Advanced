4694ab1f317f5df092d89b45897dd83bd74d2355
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-10-05 19:07:03 +0300

    Add files via upload

---
+++ C# Advanced/Functional Programming/Lab/Sum Numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(", ").Select(n => int.Parse(n)).ToArray();
+            Console.WriteLine(nums.Length);
+            Console.WriteLine(nums.Sum());
+        }
+    }
+}
