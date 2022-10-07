450ede2dc7d3d94a6f4a18c11be7e863a0ac4ca9
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-10-07 20:53:09 +0300

    Add files via upload

---
+++ C# Advanced/Functional Programming/Lab/Add VAT/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Add_VAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<double, string> vat = n => (n * 1.2).ToString("F2");
+            Console.WriteLine(string.Join("\n", Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).
+                Select(double.Parse).Select(vat)).ToArray());  
+        }
+    }
+}
