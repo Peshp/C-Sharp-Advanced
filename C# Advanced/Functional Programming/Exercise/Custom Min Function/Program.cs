5dc0cadc2c09eb3e175a99ff1a568deed3e31819
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-10-09 17:26:07 +0300

    Add files via upload

---
+++ C# Advanced/Functional Programming/Exercise/Custom Min Function/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int[], int> minNum = (nums) => nums.Min();
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(minNum(nums));
+        }
+    }
+}
