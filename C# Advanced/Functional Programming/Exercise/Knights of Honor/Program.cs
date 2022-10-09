54e68fe5f189be3177557b25f6039f6c6a232821
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-10-09 17:25:24 +0300

    Add files via upload

---
+++ C# Advanced/Functional Programming/Exercise/Knights of Honor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Knights_of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> print = (names) => Console.WriteLine($"Sir {names}");
+            string[] names = Console.ReadLine().Split();
+            foreach (var name in names)           
+                print(name);           
+        }
+    }
+}
