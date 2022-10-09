e112f6dc71b7a71d1f0c2b20178f150279a3e00e
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-10-09 17:24:43 +0300

    Add files via upload

---
+++ C# Advanced/Functional Programming/Exercise/Action Print/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Action_Point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string[]> names = (name) => Console.WriteLine(string.Join("\n", name));
+            string[] name = Console.ReadLine().Split();
+            names(name);
+        }
+    }
+}
