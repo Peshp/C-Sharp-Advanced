959b9bacae48a05bc1934dd7dfa68e93be1d08ad
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-10-11 11:35:50 +0300

    Add files via upload

---
+++ C# Advanced/Functional Programming/Exercise/Reverse And Exclude/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Reverse_And_Exclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            nums.Reverse();
+
+            int divider = int.Parse(Console.ReadLine());
+            Predicate<int> check = (num) => num % divider == 0;
+            List<int> result = new List<int>();
+
+            foreach (var num in nums)           
+                if (!check(num))
+                    result.Add(num);
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
