939f0c079cc74aa9b2133439545846b5b0b2f6e8
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-10-15 11:28:21 +0300

    Add files via upload

---
+++ C# Advanced/Defining Classes/Lab/Car/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        class Car
+        {
+            private string make;
+            private string model;
+            private int year;
+            public string Make
+            {
+                get { return this.make; }
+                set { this.make = value; }
+            }
+            public string Model
+            {
+                get { return this.model; }
+                set { this.model = value; }
+            }
+            public int Year
+            {
+                get { return this.year; }
+                set { this.year = value; }
+            }
+        }
+        static void Main()
+        {
+
+        }
+    }   
+}
