234351b8c5452b5ca7298ddab4c4d52ffea97849
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-10-19 23:22:38 +0300

    Add files via upload

---
+++ C# Advanced/Defining Classes/Exercise/Define a Class Person/Family.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public List<Person> People { get; set; }      
+        public Family()
+        {
+            People = new List<Person>();
+        }
+        public void AddPeople(Person member)
+        {
+            People.Add(member);
+        }
+        public Person Order()
+        {
+            return People.OrderBy(p => p.Name);
+        }
+    }
+}
---
+++ C# Advanced/Defining Classes/Exercise/Define a Class Person/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());                     
+            Family oldestMembers = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] token = Console.ReadLine().Split();               
+                
+                Person oldPerson = new Person(token[0], int.Parse(token[1]));
+
+                if (int.Parse(token[1]) > 30)
+                    oldestMembers.AddPeople(oldPerson);                              
+            }
+          
+            foreach (var p in oldestMembers.Order())
+            {
+
+            }
+        }
+    }
+}
