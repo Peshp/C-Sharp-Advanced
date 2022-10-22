f7a3a4ead524294820722cfa8d0b2564ac60f46a
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-10-22 15:39:31 +0300

    Add files via upload

---
+++ C# Advanced/Defining Classes/Exercise/Speed Racing/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Speed_Racing
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        private double FuelAmount { get; set; }
+        private double FuelCons { get; set; }
+        private double Travelled { get; set; }
+
+        public Car(string model, double fuel, double cons)
+        {
+            Model = model;
+            FuelAmount = fuel;
+            FuelCons = cons;
+            Travelled = 0;
+        }
+        public void Drive(double km)
+        {
+            double allkm = km * FuelCons;
+            if(FuelAmount - allkm < 0)
+                Console.WriteLine("Insufficient fuel for the drive");
+            else
+            {
+                FuelAmount -= allkm;
+                Travelled += km;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{Model} {FuelAmount:f2} {Travelled}";
+        }
+    }
+}
---
+++ C# Advanced/Defining Classes/Exercise/Speed Racing/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Speed_Racing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Car> cars = new List<Car>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] carinf = Console.ReadLine().Split();              
+                cars.Add(new Car(carinf[0], double.Parse(carinf[1]), double.Parse(carinf[2])));
+            }
+            
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "End")
+                    break;
+                string model = command.Split().Skip(1).First();                
+                double km = double.Parse(command.Split().Last());
+                Car currCar = cars.Find(c => c.Model == model);
+                currCar.Drive(km);
+            }
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
