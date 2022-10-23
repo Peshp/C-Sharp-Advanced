9ab83d638c4482bdfb52ab634dfafff60a041809
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-10-23 11:29:59 +0300

    Add files via upload

---
+++ C# Advanced/Generics/Lab/Array Creator/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    public static class ArrayCreator
+    {
+        public static T[] Create<T>(int length, T item)
+        {
+            T[] arr = new T[length];
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = item;
+            return arr;
+        }
+    }
+}
