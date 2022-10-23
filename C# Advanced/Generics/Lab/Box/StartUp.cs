96de788c52b5eec8190e7aa6147a821c1b41fb2a
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-10-23 11:29:14 +0300

    Add files via upload

---
+++ C# Advanced/Generics/Lab/Box/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxOfT
+{
+    public class StartUp
+    {
+        class Box<T>
+        {
+            private List<T> list;
+            public Box()
+            {
+                list = new List<T>();
+            }
+            public void Add(T element)
+            {
+                list.Insert(0, element);
+            }
+            public T Remove()
+            {
+                T topMost = list[0];
+                list.RemoveAt(0);
+                return topMost;
+            }
+            public int Count { get { return list.Count; } }
+        }
+        static void Main()
+        {
+            Box<int> box = new Box<int>();
+            box.Add(1);
+            box.Add(2);
+            box.Add(3);
+            Console.WriteLine(box.Remove());
+            box.Add(4);
+            box.Add(5);
+            Console.WriteLine(box.Remove());
+        }
+    }
+}
