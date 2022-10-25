e01743975bb5d22211d17a72a636039ab357fde2
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-10-25 11:57:46 +0300

    Add files via upload

---
+++ C# Advanced/Generics/Exercise/SwapMethodStrings/Box.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SwapMethodStrings
+{
+    public class Box<T>
+    {
+        public Box()
+        {
+            items = new List<T>();
+        }
+        public List<T> items { get; set; }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in items)
+            {
+                sb.Append($"{item.GetType()}: {item}\n");
+            }
+            return sb.ToString().TrimEnd();
+        }
+        public void Swap(int first, int second)
+        {
+            T temp = items[first];
+            items[first] = items[second];
+            items[second] = temp;
+        }
+    }
+}
---
+++ C# Advanced/Generics/Exercise/SwapMethodStrings/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace SwapMethodStrings
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Box<int> box = new Box<int>();
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                box.items.Add(input);
+            }
+
+            int[] indexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            box.Swap(indexes[0], indexes[1]);
+            Console.WriteLine(box);
+        }
+    }
+}
