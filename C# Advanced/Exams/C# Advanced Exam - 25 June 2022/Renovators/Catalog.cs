5afd26e762ca0ddde9d26d7665d19bb094b92758
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-10-28 19:02:33 +0300

    Add files via upload

---
+++ C# Advanced/Exams/C# Advanced Exam - 25 June 2022/Renovators/Catalog.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renovators
+{
+    public class Catalog
+    {       
+        public Catalog(string name, int neededRenovators, string project)
+        {
+            Name = name;
+            NeededRenovators = neededRenovators;
+            Project = project;
+            Renovators = new List<Renovator>();
+        }
+        public List<Renovator> Renovators;
+        public int Count { get { return Renovators.Count; } }
+        public string Name { get; set; }
+        public int NeededRenovators { get; set; }
+        public string Project { get; set; }
+        public string AddRenovator(Renovator renovator)
+        {
+            if (string.IsNullOrEmpty(renovator.Name) || string.IsNullOrEmpty(renovator.Type))
+                return $"Invalid renovator's information.";
+            if (this.Count >= NeededRenovators)
+                return "Renovators are no more needed.";
+            if (renovator.Rate > 350)
+                return "Invalid renovator's rate.";
+            Renovators.Add(renovator);
+            return $"Successfully added {renovator.Name} to the catalog.";
+        }
+        public bool RemoveRenovator(string name)
+            => Renovators.Remove(Renovators.Find(x => x.Name == name));
+        public int RemoveRenovatorBySpecialty(string type)
+            => Renovators.RemoveAll(r => r.Type == type);
+        public Renovator HireRenovator(string name)
+        {
+            Renovator renovator = Renovators.Find(r => r.Name == name);
+            if (renovator != null)
+                return renovator;
+            else
+                return null;
+        }
+        public List<Renovator> PayRenovators(int days)
+            => Renovators.FindAll(r => r.Days >= days).ToList();
+        public string Report()
+            => $"Renovators available for Project {this.Project}" +
+            Environment.NewLine +
+            string.Join(Environment.NewLine, Renovators.FindAll(r => !r.Hired));
+    }
+}
---
+++ C# Advanced/Exams/C# Advanced Exam - 25 June 2022/Renovators/Renovator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renovators
+{
+    public class Renovator
+    {
+        public Renovator(string name, string type, double rate, int days)
+        {
+            this.Name = name;
+            this.Type = type;
+            this.Rate = rate;
+            this.Days = days;
+            this.Hired = false;
+        }
+        public string Name;
+        public string Type;
+        public double Rate;
+        public int Days;
+        public bool Hired;
+        public override string ToString()      
+            =>  $"-Renovator: {this.Name}" + Environment.NewLine +
+            $"--Specialty: {this.Type}" + Environment.NewLine +
+            $"--Rate per day: {this.Rate} BGN";
+        
+    }
+}
