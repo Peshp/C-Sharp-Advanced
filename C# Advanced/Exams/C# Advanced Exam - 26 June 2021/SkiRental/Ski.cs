bba43eedec35ffc6d79cc2823f6c27bc1ecfa9a6
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-10-31 12:05:51 +0200

    Add files via upload

---
+++ C# Advanced/Exams/C# Advanced Exam - 26 June 2021/SkiRental/Ski.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkiRental
+{
+    public class Ski
+    {
+        public Ski(string manufacturer, string model, int year)
+        {
+            ManuFacturer = manufacturer;
+            Model = model;
+            Year = year;
+        }
+        public string ManuFacturer { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public override string ToString()
+        {
+            return $"{ManuFacturer} - {Model} - {Year}";
+        }
+    }
+}
---
+++ C# Advanced/Exams/C# Advanced Exam - 26 June 2021/SkiRental/SkiRental.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SkiRental
+{
+    public class SkiRental
+    {
+        private readonly List<Ski> data;
+        public SkiRental(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            data = new List<Ski>();
+        }
+        public int Count { get {return this.data.Count; } }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public void Add(Ski ski)
+        {
+            if (this.data.Count < Capacity)
+                this.data.Add(ski);
+        }
+        public bool Remove(string manufacturer, string model)
+            => this.data.Remove(this.data.Find(s => s.ManuFacturer == manufacturer && s.Model == model));
+        public Ski GetNewestSki()
+            => this.data.OrderBy(n => n.Year).First();
+        public Ski GetSki(string manufacturer, string model)
+            => this.data.Find(s => s.ManuFacturer == manufacturer && s.Model == model);
+        public string GetStatistics()
+        {
+            return $"The skis stored in {this.Name}:" +
+                Environment.NewLine +
+                string.Join(Environment.NewLine, data);
+        }
+    }
+}
