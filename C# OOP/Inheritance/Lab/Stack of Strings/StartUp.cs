3502a3139a3e0d9f3936bca9902286f5d10d6af5
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-11-11 14:06:59 +0200

    Add files via upload

---
+++ C# OOP/Inheritance/Lab/Stack of Strings/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings customStack = new StackOfStrings();
+            var a = new List<string>();
+            a.Add("a");
+            a.Add("b");
+            a.Add("c");
+            customStack.AddRange(a);
+            Console.WriteLine(customStack.IsEmpty());
+            Console.WriteLine(string.Join("", customStack));
+        }
+    }
+}
