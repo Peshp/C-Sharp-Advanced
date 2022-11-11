3c109e4b580a8fc102620acc403913fa84930ee9
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-11-11 14:01:55 +0200

    Add files via upload

---
+++ C# OOP/Inheritance/Lab/Random List/RandomList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public RandomList()
+        {
+            rnd = new Random();
+        }
+        private Random rnd;
+        public string RandomString()
+        {
+            int index = rnd.Next(0, this.Count);
+            string str = this[index];
+            this.RemoveAt(index);
+            return str;
+        }          
+    }
+}
---
+++ C# OOP/Inheritance/Lab/Random List/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList rndList = new RandomList();
+            rndList.Add("Shte");
+            rndList.Add("eba");
+            rndList.Add("judge e hubav");
+            rndList.Add("na judge");
+            rndList.Add("maikata");
+            rndList.RandomString();
+            Console.WriteLine(string.Join(" ", rndList));
+        }
+    }
+}
