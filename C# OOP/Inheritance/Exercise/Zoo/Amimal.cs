a32cd12d9a866d0f2027cd16229e76af484856eb
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-11-12 15:18:09 +0200

    Add files via upload

---
+++ C# OOP/Inheritance/Exercise/Zoo/Amimal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo
+{
+    public class Animal
+    {
+        protected string name;
+        public Animal(string name)
+        {
+            this.name = name;
+        }
+        public string Name
+        {
+            get { return this.Name; }
+            set { this.Name = value; }
+        }
+    }
+}
---
+++ C# OOP/Inheritance/Exercise/Zoo/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Zoo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Snake snake = new Snake(name);
+
+            string name2 = Console.ReadLine();
+            Bear bear = new Bear(name2);
+
+            Console.WriteLine($"{snake} VS {bear}");
+        }
+    }
+}
