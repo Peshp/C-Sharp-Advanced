af3e2bb98bec5ac82b0b2a56f72eaa50d70a45ec
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-11-17 18:33:31 +0200

    Add files via upload

---
+++ C# OOP/Encapsulation/Lab/First and Reserve Team/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;    
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; private set; }      
+    }
+}
---
+++ C# OOP/Encapsulation/Lab/First and Reserve Team/Team.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Team
+    {    
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reverseTeam;
+        public Team(string name)
+        {
+            this.name = name;
+            this.firstTeam = new List<Person>();
+            this.reverseTeam = new List<Person>();
+        }
+        public string Name => this.name;
+        public IReadOnlyCollection<Person> FirstTeam => this.firstTeam;
+        public IReadOnlyCollection<Person> ReverseTeam => this.reverseTeam;
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+                firstTeam.Add(person);
+            else
+                reverseTeam.Add(person);
+        }
+    }
+}
