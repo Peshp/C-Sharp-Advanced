125264cf6160bd5c30c379a5e90e2004d8c4d700
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-11-19 16:20:17 +0200

    Add files via upload

---
+++ C# OOP/Encapsulation/Exercise/Pizza Calories/Dough.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaCalories
+{
+    public class Dough
+    {
+        private string flourType;
+        private string baking;
+        private double grams;
+        public string FlourType
+        {
+            get { return this.flourType; }
+            private set
+            {
+                if (value == "White" || value == "Wholegrain")
+                    this.flourType = value;
+                else
+                    throw new ArgumentException("Invalid type of dough.");
+            }
+        }
+        public string Baking
+        {
+            get { return this.baking; }
+            private set
+            {
+                if (value == "Crispy" || value == "Chewy" || value == "Homemade")
+                    this.baking = value;
+                else
+                    throw new Exception("Invalid type of dough.");
+            }
+        }
+        public double Grams
+        {
+            get { return this.grams; }
+            private set
+            {
+                if (value > 1 && value < 200)
+                    this.grams = value;
+                else
+                    throw new Exception("Dough weight should be in the range [1..200].");
+            }
+        }
+        public Dough(string flour, string bake, double grams)
+        {
+            this.FlourType = flour;
+            this.Baking = bake;
+            this.Grams = grams;
+        }
+    }
+}
---
+++ C# OOP/Encapsulation/Exercise/Pizza Calories/StartUp.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace PizzaCalories
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            try
+            {
+                while (true)
+                {                                     
+                    double grams = DefineGrams(input);
+                    double grams2 = DefineGrams2(input);
+                    double grams3 = double.Parse(input.Split()[3]);
+                    Dough dough = new Dough(input.Split()[1], input.Split()[2], grams3);
+                    input = Console.ReadLine();
+                    if (input == "END")
+                    {
+                        Console.WriteLine($"{(grams3 * 2 * grams2 * grams):f2}");
+                        break;
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+        }
+
+        private static double DefineGrams2(string input)
+        {
+            double grams2 = 0;
+            switch (input.Split()[2])
+            {
+                case "Crispy": grams2 = 0.9; break;
+                case "Chewy": grams2 = 1.1; break;
+                case "Homemade": grams2 = 1; break;
+            }
+            return grams2;
+        }
+
+        private static double DefineGrams(string input)
+        {
+            double grams = 0;
+            switch (input.Split()[1])
+            {
+                case "White": grams = 1.5; break;
+                case "Wholegrain": grams = 1; break;
+            }
+            return grams;
+        }
+    }
+}
