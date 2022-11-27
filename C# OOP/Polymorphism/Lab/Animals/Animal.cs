1dbf894fde86d90b748896070464f85f16a731eb
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-11-27 18:22:54 +0200

    Add files via upload

---
+++ C# OOP/Polymorphism/Lab/Animals/Animal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+        public Animal(string name, string favouriteFood)
+        {
+            this.Name = name;
+            this.FavouriteFood = favouriteFood;
+        }
+        public virtual string Name { get; set; }
+        public virtual string FavouriteFood { get; set; }
+        public virtual string ExplainSelf()
+         => $"I am {this.Name} and my fovourite food is {this.FavouriteFood}";
+    }
+}
---
+++ C# OOP/Polymorphism/Lab/Animals/Dog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Dog : Animal
+    {
+        public Dog(string name, string favouriteFood) : base(name, favouriteFood)
+        { }
+        public override string ExplainSelf()
+         => $"I am {this.Name} and my fovourite food is {this.FavouriteFood}" +
+            "\n" +
+            "BARK";
+    }
+}
