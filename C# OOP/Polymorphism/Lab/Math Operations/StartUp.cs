c12a8bc93d5c8a5cc8512984babab8ecec563b4b
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-12-11 14:16:53 +0200

    Add files via upload

---
+++ C# OOP/Polymorphism/Lab/Math Operations/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Operations
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var mo = new MathOperations();
+            Console.WriteLine(mo.Add(2, 3));
+            Console.WriteLine(mo.Add(2.3, 3.2, 4.23));
+            Console.WriteLine(mo.Add(2.3m, 3.2m, 4.23m));
+        }
+    }
+}
