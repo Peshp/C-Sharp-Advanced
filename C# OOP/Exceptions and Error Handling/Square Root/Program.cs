ba99bcae365551b10c31d19ed6f67abcde801e1b
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-12-17 12:49:11 +0200

    Add files via upload

---
+++ C# OOP/Exceptions and Error Handling/Square Root/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Square_Root
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            try
+            {
+                if (number < 0)
+                    throw new ArgumentException("Invalid number.");
+                else
+                    Console.WriteLine(Math.Sqrt(number)); 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
