1d164a46b847665333b94d921b71b60e4ea79c75
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-12-19 23:35:41 +0200

    Add files via upload

---
+++ C# OOP/Exams/C# OOP Exam – 14 Aug 2022/Business logic/Models/MilitaryUnits/MilitaryUnit.cs
@@ -0,0 +1,45 @@
+﻿using PlanetWars.Models.MilitaryUnits.Contracts;
+using PlanetWars.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetWars.Models.MilitaryUnits
+{
+    public abstract class MilitaryUnit : IMilitaryUnit
+    {
+        private double cost;
+        private int enduranceLevel;
+        public MilitaryUnit(double cost)
+        {
+            Cost = cost;
+            enduranceLevel = 1;
+        }
+        public double Cost
+        {
+            get => cost;
+            private set { cost = value; }
+        }
+
+        public int EnduranceLevel
+        {
+            get => enduranceLevel;
+            private set
+            {
+                if (value > 20)
+                {
+                    value = 20;
+                    enduranceLevel = 20;
+                    throw new ArgumentException(ExceptionMessages.EnduranceLevelExceeded);
+                }
+                else
+                    enduranceLevel = value;
+            }
+        }
+
+        public void IncreaseEndurance()
+        {
+            EnduranceLevel += 1;
+        }
+    }
+}
