c03fb836c892b9fde27140aa1568584e7374782e
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-12-21 15:08:44 +0200

    Add files via upload

---
+++ C# OOP/Exams/C# OOP Exam - 09 April 2022/Structure/Models/Contracts/Race.cs
@@ -0,0 +1,62 @@
+﻿using Formula1.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Formula1.Models.Contracts
+{
+    public class Race : IRace
+    {
+        private string raceName;
+        private int numberOfLaps;
+        private List<IPilot> pilots;
+        public Race(string raceName, int numberOfLaps)
+        {
+            RaceName = raceName;
+            NumberOfLaps = numberOfLaps;
+            TookPlace = false;
+            pilots = new List<IPilot>();
+        }
+        public string RaceName
+        {
+            get => raceName;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length <= 5)
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidRaceName, value));
+                raceName = value;
+            }
+        }
+
+        public int NumberOfLaps
+        {
+            get => numberOfLaps;
+            private set
+            {
+                if (value <= 1)
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidLapNumbers, value));
+                numberOfLaps = value;
+            }
+        }
+
+        public bool TookPlace { get; set; }
+
+        public ICollection<IPilot> Pilots => pilots;
+
+        public void AddPilot(IPilot pilot)
+        {
+            pilots.Add(pilot);
+        }
+
+        public string RaceInfo()
+        {
+            string took = TookPlace == true ? "Yes" : "No";
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"The {RaceName} race has:");
+            sb.AppendLine($"Participants: {pilots.Count}");
+            sb.AppendLine($"Number of laps: {NumberOfLaps}");
+            sb.AppendLine($"Took place: {took}");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
