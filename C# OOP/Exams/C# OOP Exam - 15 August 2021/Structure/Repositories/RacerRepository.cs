2f980648d9b0a3837624bc7c3ec11dad397164ab
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-12-25 20:24:36 +0200

    Add files via upload

---
+++ C# OOP/Exams/C# OOP Exam - 15 August 2021/Structure/Repositories/RacerRepository.cs
@@ -0,0 +1,33 @@
+﻿using CarRacing.Models.Racers.Contracts;
+using CarRacing.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRacing.Repositories
+{
+    public class RacerRepository : IRepository<IRacer>
+    {
+        private List<IRacer> models;
+        public RacerRepository()
+        {
+            models = new List<IRacer>();
+        }
+        public IReadOnlyCollection<IRacer> Models => models.AsReadOnly();
+
+        public void Add(IRacer model)
+        {
+            models.Add(model);
+        }
+
+        public IRacer FindBy(string property)
+        {
+            return models.Find(x => x.Username == property);
+        }
+
+        public bool Remove(IRacer model)
+        {
+            return models.Remove(model);
+        }
+    }
+}
